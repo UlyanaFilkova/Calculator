ef165cf57d031651d5b65985689604754c2aa624
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-10-04 14:52:32 +0300

    debug float calculations  problem

--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,14 +1,24 @@
 export default function calculate(num1, operation, num2) {
+  result = 0;
   switch (operation) {
     case "+":
-      return parseFloat(num1) + parseFloat(num2);
+      result = parseFloat(num1) + parseFloat(num2);
+      break;
     case "-":
-      return parseFloat(num1) - parseFloat(num2);
+      result = parseFloat(num1) - parseFloat(num2);
+      break;
     case "*":
-      return parseFloat(num1) * parseFloat(num2);
+      result = parseFloat(num1) * parseFloat(num2);
+      break;
     case "/":
-      return parseFloat(num1) / parseFloat(num2);
+      result = parseFloat(num1) / parseFloat(num2);
+      break;
     default:
       return 0;
   }
+  // используем toFixed() и replace(), чтобы разобраться с проблемой арифметики чисел с плавающей точкой
+  // оставим 10 знаков после запятой, всё что дальше за ними - округлим
+  // с помощью /0+$/ убираем нули после запятой, если за ними ничего не следует
+  // а затем с помощью /\.$/ убираем саму запятую, если за ней ничего не следует
+  return result.toFixed(10).replace(/0+$/, "").replace(/\.$/, "");
 }
